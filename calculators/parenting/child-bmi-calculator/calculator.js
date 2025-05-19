4a69858fd247e1bf15f999b11839f5f8be45d5f3
beakbreader <jimmy4374@outlook.com>
2025-05-19 23:17:09 +1000

    Delete calculators directory

--- calculators/parenting/child-bmi-calculator/calculator.js
+++
@@ -1,9 +0,0 @@
-
-function calcChildBMI() {
-  const weight = parseFloat(document.getElementById('weight').value);
-  const height = parseFloat(document.getElementById('height').value) / 100;
-  const bmi = weight / (height * height);
-  document.getElementById('result').innerText = isFinite(bmi)
-    ? "Estimated Child BMI: " + bmi.toFixed(2)
-    : "Enter valid numbers.";
-}
